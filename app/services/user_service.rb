4c63341de29757c2a34f4302b9eb03ee6a77bf6c
Jean Tessier <jean.tessier@gusto.com>
2021-07-26 14:45:54 -0700

    Add example without transactional context

--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -24,4 +24,12 @@ class UserService
       after_commit { puts 'All is well.' }
     end
   end
+
+  def without_transaction(*args)
+    User.create! name: 'abc', age: 123
+    User.create! name: 'def', age: 456
+    after_commit { puts 'Created 2 new users!' }
+    User.create! *args
+    after_commit { puts 'Created 2 new users!' }
+  end
 end
